a3541c3171abceb9c8a32cd1d74d51ec33bb481e
Dzmitry <43175807+dzmitry-karachun-1987@users.noreply.github.com>
2018-09-30 21:57:31 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  var i = 0;
-  var count = 0;
-  for(i=0;i<preferences.length;i++){
-    var first = preferences[i];
-    var second = preferences[first-1]
+  let i = 0;
+  let count = 0;
+  for(i = 0, len = preferences.length;i < len; i++){
+    let first = preferences[i];
+    let second = preferences[first-1]
     if(preferences[second-1]== i+1) count ++;
   }
   return Math.floor(count/3);
-
-};
+}
